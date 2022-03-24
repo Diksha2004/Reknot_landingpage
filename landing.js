57d8ea269e8a3408897b05a7d2d7af6ece67067a
Diksha2004 <101046418+Diksha2004@users.noreply.github.com>
2022-03-24 16:46:32 +0530

    adding js to up arrow button

--- landing.js
+++ landing.js
@@ -1,3 +1,4 @@
+//nav bar
 function myFunction() {
     var x = document.getElementById("myTopnav");
     if (x.className === "topnav") {
@@ -6,3 +7,30 @@ function myFunction() {
       x.className = "topnav";
     }
   }
+
+
+  
+let mybutton = document.getElementById("btn-back-to-top");
+
+
+window.onscroll = function () {
+    scrollFunction();
+  };
+
+    function scrollFunction() {
+     if (
+            document.body.scrollTop > 20 ||
+            document.documentElement.scrollTop > 20
+       ) {
+             mybutton.style.display = "block";
+       } else {
+                 mybutton.style.display = "none";
+       }
+  }
+
+   mybutton.addEventListener("click", backToTop);
+
+     function backToTop() {
+        document.body.scrollTop = 0;
+        document.documentElement.scrollTop = 0;
+     }
